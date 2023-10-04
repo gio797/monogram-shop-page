53b1cd7451507aa8982df13b846ef5f6bcbb3ee2
gio797 <g.muchaidze@yahoo.com>
2023-10-04 15:19:10 +0400

    first commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./header.css";
+import { RxMix } from "react-icons/rx";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
+function Header() {
+  const [navOpen, setNavOpen] = useState(false);
+  return (
+    <header>
+      <div>
+        <a href="/" className="logo">
+          <RxMix size={40} /> MONOGRAM
+        </a>
+      </div>
+      <nav className={`nav ${navOpen && "nav-open"}`}>
+        <button className="close-nav" onClick={() => setNavOpen(false)}>
+          <AiOutlineClose size={25} />
+        </button>
+        <ul className="menu-list">
+          <li>
+            <a href="">How it works</a>
+          </li>
+          <li>
+            <a href="">Workflow</a>
+          </li>
+          <li>
+            <a href="">download</a>
+          </li>
+          <li>
+            <a href="">blog</a>
+          </li>
+          <li>
+            <a href="">support</a>
+          </li>
+          <li>
+            <a href="">shop</a>
+          </li>
+        </ul>
+      </nav>
+      <button className="open-nav" onClick={() => setNavOpen(true)}>
+        <AiOutlineMenu size={25} />
+      </button>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,17 @@
+import "./hero.css";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div>
+        <h1>
+          A CONSOLE FOR <br />
+          EVERY WORKFLOW
+        </h1>
+        <p>Discover the perfect console for yours.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
