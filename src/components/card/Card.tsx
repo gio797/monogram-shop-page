f831f98a783b960de093bb052ffee64b1611832d
gio797 <g.muchaidze@yahoo.com>
2023-10-05 10:03:09 +0400

    add button to card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./card.css";
 
 type cardProps = {
@@ -8,8 +9,13 @@ type cardProps = {
 };
 
 function Card({ cardImg, name, price, info }: cardProps) {
+  const [showButton, setShowButton] = useState(false);
   return (
-    <div className="card">
+    <div
+      className="card"
+      onMouseEnter={() => setShowButton(true)}
+      onMouseLeave={() => setShowButton(false)}
+    >
       <div className="pre-order">
         <span>PRE-ORDER</span>
       </div>
@@ -19,6 +25,7 @@ function Card({ cardImg, name, price, info }: cardProps) {
         <p>{price}</p>
       </div>
       <p>{info}</p>
+      {showButton ? <button>SHOP NOW</button> : null}
     </div>
   );
 }
